1ed93ca4356e27591f167f6ea812230664b45248
kmnrm <58434461+kmnrm@users.noreply.github.com>
2020-03-04 00:20:13 +0800

    Add filters for Flat Admin
    Enable filter through new buildings.
    Enable filter by rooms number.
    Enable filter through have balcony flats.

--- property/admin.py
+++ property/admin.py
@@ -7,5 +7,6 @@ class FlatAdmin(admin.ModelAdmin):
     readonly_fields = ['created_at']
     list_display = ('town', 'address', 'price', 'new_building', 'construction_year',)
     list_editable = ['new_building']
+    list_filter = ('new_building', 'rooms_number', 'has_balcony',)
 
 admin.site.register(Flat, FlatAdmin)
