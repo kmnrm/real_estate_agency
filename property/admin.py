fbc5b574516a8325379265df7d1e4bb0358291a7
kmnrm <58434461+kmnrm@users.noreply.github.com>
2020-03-04 00:51:16 +0800

    Enable likes
    Add field Flat.liked_by.
    Make liked_by field an Input widget.

--- property/admin.py
+++ property/admin.py
@@ -8,6 +8,7 @@ class FlatAdmin(admin.ModelAdmin):
     list_display = ('town', 'address', 'price', 'new_building', 'construction_year',)
     list_editable = ['new_building']
     list_filter = ('new_building', 'rooms_number', 'has_balcony',)
+    raw_id_fields = ('liked_by',)
 
 class ComplaintAdmin(admin.ModelAdmin):
     raw_id_fields = ('flat', 'complainant',)
--- property/models.py
+++ property/models.py
@@ -22,7 +22,8 @@ class Flat(models.Model):
     active = models.BooleanField("Активно-ли объявление", db_index=True)
     construction_year = models.IntegerField("Год постройки здания", null=True, blank=True, db_index=True)
     new_building = models.NullBooleanField("Новостройка")
-
+    liked_by = models.ManyToManyField(User, verbose_name="Кто лайкнул", related_name='liked_flats', blank=True)
+    
     def __str__(self):
         return f"{self.town}, {self.address} ({self.price}р.)"
 
